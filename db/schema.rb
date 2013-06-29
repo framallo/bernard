4c6f725eed80d3240193e58d3fcc8c504bff64e1
Federico Ramallo <framallo@gmail.com>
2013-06-29 11:05:49 -0500

    added schema

--- db/schema.rb
+++ db/schema.rb
@@ -35,9 +35,21 @@ ActiveRecord::Schema.define(version: 20130531010552) do
   end
 
   create_table "transactions", force: true do |t|
+    t.boolean  "pm_deleted"
+    t.integer  "pm_timestamp"
+    t.integer  "pm_type"
+    t.integer  "pm_account_id"
+    t.text     "pm_payee"
+    t.decimal  "pm_sub_total",           precision: 10, scale: 2
+    t.text     "pm_of_x_id"
+    t.binary   "pm_image"
+    t.text     "pm_server_id"
+    t.text     "pm_overdraft_id"
+    t.datetime "date"
     t.integer  "account_id"
-    t.datetime "created_at"
-    t.integer  "check_number"
+    t.boolean  "deleted"
+    t.text     "check_number"
+    t.text     "payee_name"
     t.integer  "payee_id"
     t.integer  "category_id"
     t.integer  "department_id"
@@ -45,8 +57,10 @@ ActiveRecord::Schema.define(version: 20130531010552) do
     t.decimal  "amount"
     t.boolean  "cleared"
     t.string   "currency_id"
+    t.text     "uuid"
     t.decimal  "currency_exchange_rate", precision: 10, scale: 2
     t.decimal  "balance",                precision: 10, scale: 2
+    t.datetime "created_at"
     t.datetime "updated_at"
   end
 

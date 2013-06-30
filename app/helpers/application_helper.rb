dca982b7055c3b14df680292d713e22485f69416
Federico Ramallo <framallo@gmail.com>
2013-06-30 18:17:51 -0500

    improved transacation view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def render_transactions transactions
     render partial: '/transactions/transactions', locals: {transactions: transactions} 
   end
+
+  def cleared_icon(cleared)
+    content_tag('i', '', class: cleared ? 'icon-ok' : 'icon-remove')
+  end
 end

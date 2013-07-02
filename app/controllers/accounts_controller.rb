a429e4ceb4516b701d007abc308097b97419d176
Federico Ramallo <framallo@gmail.com>
2013-07-02 08:25:28 -0500

    cleaned up controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,14 +1,5 @@
 class AccountsController < ApplicationController
 
-  # pm_types
-  # 0 Withdrawal
-  # 1 Deposit
-  # 2 Transfer
-  # 3 
-  # 4
-  # 5 Dunno
-  #
-
   def index
     @accounts = Account.all
   end

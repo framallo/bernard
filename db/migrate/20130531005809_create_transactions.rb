21813f697a015c21e3d8984b2caee0b553c7243c
Federico Ramallo <framallo@gmail.com>
2013-07-06 19:54:15 -0500

    improved categories migration

--- db/migrate/20130531005809_create_transactions.rb
+++ db/migrate/20130531005809_create_transactions.rb
@@ -19,13 +19,9 @@ class CreateTransactions < ActiveRecord::Migration
       t.integer :payee_id
       t.integer :category_id
       t.integer :department_id
-      #t.text    :memo
       t.decimal :amount
       t.boolean :cleared
-      #t.string :currency_id
-      t.text    :uuid
-      #t.decimal :currency_exchange_rate, :precision => 10, :scale => 2
-      #t.decimal :balance, :precision => 10, :scale => 2
+      t.string    :uuid
 
       t.timestamps
     end

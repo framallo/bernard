80bb10e1b72130c9575a01f541069e28f489c5f2
Federico Ramallo <framallo@gmail.com>
2013-07-12 18:43:10 -0500

    added model relations between split and transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -8,15 +8,23 @@ class Transaction < ActiveRecord::Base
   # 5 Dunno but removed it for now
   #
 
-  default_scope { order('date desc').where('pm_type <> 5') }
-
   scope :search, ->(q) { where "payee_name like ? OR uuid = ?", "%#{q}%", q }
   scope :uuid, ->(uuid) { where(uuid:uuid).first }
-  scope :active, ->     { where(deleted:false) }
+  scope :active, ->     { where(deleted:false).order('date desc').where('pm_type <> 5').includes(:splits) }
   scope :cleared, ->     { where(cleared:true) }
   scope :before_today, ->     { where('date < ?', Time.now) }
   scope :balance, -> { select('*').select('SUM(amount) OVER (PARTITION BY account_id ORDER BY date ASC) as balance') }
 
   belongs_to :account
+  has_many :splits
+
+
+  def split?
+    @split ||= splits.size > 1
+  end
+
+  def category_name
+    split? ? '<--Splits-->' : splits.first.category.try(:name)
+  end
 
 end

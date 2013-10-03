d45a5c7b049c460d70374c6d51a43d750fdb95e0
Roberto Ramallo <r.ramallo@gmail.com>
2013-10-03 16:26:32 -0500

    added group in transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,7 +4,7 @@ class TransactionsController < ApplicationController
   # GET /transactions
   # GET /transactions.json
   def index
-    @transactions = Transaction.active.sort
+    @transactions = Transaction.active
   end
 
   # GET /transactions/1

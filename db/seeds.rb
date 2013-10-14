00136af25831f65b4a68a9e106d9232dabfb7d2b
Jesus Romero <jose.romero@tangosource.com>
2013-10-14 14:07:22 -0500

    created seeds to bernard application

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,125 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-Account.create(
-  deleted: false
-  pm_id: 1
-  pm_acunt_type: 1
+boolean = [true, false]
+currency = ["AED", "ALL", "CAD", "CNY", "MXN", "IRR", "JPY", "USD", "UYU"]
+#Seed to Account table
+5.times do
+  Account.create(
+    deleted: false,
+    pm_id: rand(0..8),
+    pm_account_type: rand(0..8),
+    display_order: rand(1..4),
+    name: Faker::Company.name,
+    balance_overall: rand*(5),
+    balance_cleared: rand*(5),
+    number: Faker::Number.number(2),
+    institution: Faker::Company.suffix,
+    phone: Faker::PhoneNumber.cell_phone,
+    expiration_date: Faker::Business.credit_card_expiry_date.strftime("%d,%m,%Y"),
+    check_number: Faker::Number.digit,
+    notes: Faker::Lorem.paragraph,
+    pm_icon: "image",
+    url: Faker::Internet.url,
+    of_x_id: "dummy",
+    of_x_url: Faker::Internet.domain_word,
+    password: Faker::Internet.password,
+    fee: rand*(100),
+    fixed_percent: rand*(20),
+    limit_amount: 1000 + rand*(9_000),
+    limit: boolean.sample,
+    total_worth: boolean.sample,
+    exchange_rate: rand*(10),
+    currency_code: currency.sample,
+    last_sync_time: (Time.now - rand(60).days).to_date,
+    routing_number: rand(100),
+    overdraft_account_id: rand(30).to_s,
+    keep_the_change_account_id: rand(30).to_s,
+    heek_change_round_to: rand*(5),
+    uuid: Faker::Code.isbn(64)
+  )
+  #seed to category table
+  Category.create(
+    name: Faker::Commerce.department, 
+    deleted: boolean.sample,
+    pm_id: rand(0..8),
+    pm_type: rand(0..8),
+    budget_period: rand(100..10000),
+    budget_limit: rand(500..5000),
+    include_subcategories: boolean.sample,
+    rollover: boolean.sample,
+    uuid: Faker::Code.isbn
+  )
+  #seed to department table
+  Department.create(
+    name: Faker::Commerce.product_name,
+    pm_id: rand(0..8),
+    uuid: Faker::Code.isbn,
+    deleted: boolean.sample
+  )
+  #seed to Payee table
+  Payee.create(
+    name: Faker::Name.name,
+    deleted: boolean.sample,
+    pm_id: rand(0..8),
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
+    uuid: Faker::Code.isbn
+  )
+end
+
+10.times do
+
+  pm = rand(0..2)
+  pm_id = rand(0..8)
+  account = Account.all.map(&:id).sample
+  payee = Payee.all.map(&:id).sample
   
+  #seed to transaction table
+  Transaction.create(
+    pm_type: pm,
+    pm_id: pm_id,
+    account_id: account,
+    pm_account_id: account,
+    pm_payee: Payee.find(payee).name,
+    pm_sub_total: rand*(1_000),
+    pm_of_x_id: "dummy",
+    pm_image: "dummy",
+    pm_overdraft_id: rand(30).to_s,
+    date: Time.now - rand(0..60).days,
+    deleted: boolean.sample,
+    check_number: rand(10).to_s,
+    payee_name: Payee.find(payee).name,
+    payee_id: payee,
+    category_id: Category.all.map(&:id).sample,
+    department_id: Department.all.map(&:id).sample,
+    amount: 10 + rand*(10_000),
+    cleared: boolean.sample,
+    uuid: Faker::Code.isbn
+  )
+
+end
+3.times do
+  transaction_id = Transaction.all.map(&:id).sample
+  transaction = Transaction.find(transaction_id) 
+  #seed to split table
+  Split.create(
+    pm_id: transaction.pm_id,
+    transaction_id: transaction_id,
+    amount: transaction.amount,
+    xrate: rand*(5),
+    category_id: transaction.category_id,
+    class_id: rand(0..10),
+    memo: Faker::Lorem.paragraph,
+    transfer_to_account_id: rand(100),
+    currency_code: currency.sample,
+    of_x_id: "dummy"
+  )
+end
+
+
+
+
+
+
+
+
+
 

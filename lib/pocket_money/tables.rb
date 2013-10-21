b9b05aba210eaa6ad22269a14538b52a6c28281e
Jesus Romero <elherzt@gmail.com>
2013-10-21 13:37:40 -0500

    add validation for test enviroment

--- lib/pocket_money/tables.rb
+++ lib/pocket_money/tables.rb
@@ -1,8 +1,11 @@
-
 class PocketMoney
   module BaseTable
     def self.included(main)
-      main.establish_connection(adapter: :sqlite3, database: APP_CONFIG['pocket_money_database'], encoding:'iso-8859')
+      if ENV["RAILS_ENV"] != 'test'
+        main.establish_connection(adapter: :sqlite3, database: APP_CONFIG['pocket_money_database'], encoding:'iso-8859')
+      else
+        main.establish_connection(adapter: :sqlite3, database: APP_CONFIG['pocket_money_database_test'], encoding:'iso-8859')
+      end
       main.table_name = main.name.demodulize.sub(/(\w)/) {|w| w.downcase}
       main.inheritance_column = "rails_type"
     end
@@ -10,7 +13,6 @@ class PocketMoney
 
   class Accounts < ActiveRecord::Base
     include BaseTable
-
   end
   class Categories < ActiveRecord::Base
     include BaseTable

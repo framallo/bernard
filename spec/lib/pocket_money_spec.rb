f78f9d5c0b178bbab9249812cac26b16a0c7ad19
Jesus Romero <elherzt@gmail.com>
2013-10-23 11:16:06 -0500

    refactored tests

--- spec/lib/pocket_money_spec.rb
+++ spec/lib/pocket_money_spec.rb
@@ -5,8 +5,13 @@ require 'pocket_money'
 Bernard::Application.load_tasks
 describe PocketMoney do
   context "import data for a known database" do
-    accounts    = Account.all
-    transactions = Transaction.all 
+    accounts               = Account.all
+    transactions           = Transaction.all 
+    categories             = Category.all
+    payees                 = Payee.all
+    departments            = Department.all
+    splits                 = Split.all
+    repeating_transactions = RepeatingTransaction.all
 
     before (:all) do
       Rake::Task['db:drop'].invoke
@@ -24,11 +29,11 @@ describe PocketMoney do
     end
 
     it "should have $1,650.00 current balance in 'cuenta ahorro'" do
-      (Account.find_by_name("Cuenta Ahorro").current_balance.to_f).should eq(1650)
+      (accounts.find_by_name("Cuenta Ahorro").current_balance.to_f).should eq(1150)
     end
     
     it "the second account should have a limit of $100" do
-      (Account.find(2).limit_amount.to_f).should eq(100)
+      (accounts[1].limit_amount.to_f).should eq(100)
     end
 
     it "all limit amount sum $600" do 
@@ -36,11 +41,11 @@ describe PocketMoney do
     end
 
     it "should have $740 current balance in 'efectivo'" do
-      (Account.find_by_name("Efectivo").current_balance.to_f).should eq(740)
+      (accounts.find_by_name("Efectivo").current_balance.to_f).should eq(1240)
     end
 
     it "should have $350 current balance in 'tarjeta'" do
-      (Account.find_by_name("Tarjeta").current_balance.to_f).should eq(350)
+      (accounts.find_by_name("Tarjeta").current_balance.to_f).should eq(350)
     end   
 
     #table transactions
@@ -49,7 +54,7 @@ describe PocketMoney do
     end
 
     it "the first transaction should have $1750 of amount" do
-      (Transaction.first.amount.to_f).should eq(1750)
+      (transactions.first.amount.to_f).should eq(1750)
     end
 
     it "should have transactions with transaction type" do
@@ -80,106 +85,106 @@ describe PocketMoney do
     end
     
     it "first transaction should have category id 16" do
-      transaction = Transaction.first
+      transaction = transactions.first
       (transaction.category_id).should eq(16)
     end
 
     it "first transaction should have class personal" do
-      transaction = Transaction.first 
+      transaction = transactions.first 
       (transaction.department_id).should eq(1)
     end
 
     #table split
     it "all aplits should have a transaction id" do
-      all_splits = Split.all.map(&:transaction_id)
+      all_splits = splits.map(&:transaction_id)
       all_splits.include?(nil).should eq(false)
       all_splits.include?("").should eq(false)
     end
 
     it "all splits should have a transaction with currency code" do
-      currency_codes = Split.all.map(&:currency_code) 
+      currency_codes = splits.map(&:currency_code) 
       currency_codes.include?(nil).should eq(false)
       currency_codes.include?("").should eq(false)
     end
 
     it "the sum of the splits of a transactions should have same amount that transaction " do
       35.times do |count|
-        transaction = Transaction.find(count+1)
+        transaction = transactions[count]
         splits = transaction.splits
         (splits.map(&:amount).sum.to_f).should eq(transaction.amount)
       end
     end
     
     it "first split should have a class id 1" do
-      split = Split.first 
+      split = splits.first 
       (split.class_id).should eq(1)
     end
 
     it "amount of  transaction 2  should equal at transaction 37" do
-      transaction_2  = Transaction.find(2).amount.to_f
-      transaction_37 = Transaction.find(37).amount.to_f
+      transaction_2  = transactions[1].amount.to_f
+      transaction_37 = transactions[36].amount.to_f
       transaction_2.should eq(transaction_37)
     end
 
     it "amount of  transaction 4  should equal at transaction 36" do
-      transaction_4  = Transaction.find(4).amount.to_f
-      transaction_36 = Transaction.find(36).amount.to_f
+      transaction_4  = transactions[3].amount.to_f
+      transaction_36 = transactions[35].amount.to_f
       transaction_4.should eq(transaction_36)
     end
 
     #table department <--- class
     it "should have personal and negocios classes" do
-      (Department.all.map(&:name)).should eq(["Personal", "Negocios"])
+      (departments.map(&:name)).should eq(["Personal", "Negocios"])
     end
 
     it "all class should have a uuid" do 
-      departments = Department.all.map(&:uuid)
+      departments = departments.map(&:uuid)
       departments.include?(nil).should eq(false)
       departments.include?("").should eq(false)
     end
 
     #table category
     it "should have 20 Categories" do
-      (Category.all.count).should eq(20)
+      (categories.count).should eq(20)
     end
 
     it "all categories should have a uuid" do 
-      uuids = Category.all.map(&:uuid)
+      uuids = categories.map(&:uuid)
       uuids.include?(nil).should eq(false)
       uuids.include?("").should eq(false)
     end
 
     it "should include 'Salario' category" do
-      names = Category.all.map(&:name)
+      names = categories.map(&:name)
       names.include?("Salario").should eq(true)
     end
 
     #table Payee 
     it "should have 9 payees" do
-      (Payee.all.count).should eq(9)
+      (payees.count).should eq(9)
     end
     
     it "should include 'TangoSource' payee" do
-      names = Payee.all.map(&:name)
+      names = payees.map(&:name)
       names.include?("TangoSource").should eq(true)
     end
 
     it "Last payee should be 'Ahorro' " do
-      (Payee.last.name).should eq("Ahorro")
+      (payees.last.name).should eq("Ahorro")
     end
 
     #table repeating_transactions
     it "should have 2 repeating transactions" do
-      (RepeatingTransaction.all.count).should eq(2)
+      (repeating_transactions.count).should eq(2)
     end
 
     it "last repeating transaction should have a valid date" do
-      transaction = RepeatingTransaction.last
+      transaction = repeating_transactions.last
       (transaction.end_date.acts_like?(:time)).should eq(true)
     end
 
     it "transaction 36 should have pm_type 5" do
-      transaction = Transaction.find(36)
+      transaction = transactions[35]
       (transaction.pm_type).should eq(5)
     end
 

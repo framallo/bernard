e0e9e09b84a13e8a8bc34574e86cd87d8cf2793d
Jesus Romero <elherzt@gmail.com>
2013-10-23 18:32:49 -0500

    added some test

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
 
 describe Transaction do
-  let(:transaction) {create(:transaction)}
+  transactions = Transaction.all
+
+  it { should belong_to(:account) }
+
+  it { should have_many(:splits) }
 
   it "has a valid transaction" do
-    transaction.should be_valid
+    (transactions.first).should be_valid
+  end
+  
+  #scopes
+  it "should have 33 active transactions" do
+    transactions.active.count.should eq(33)
+  end
+
+  it "should find a transaction given a uuid" do
+    (transactions.search("TangoSource").count).should eq(2)
+  end
+
+  it "" do
   end
 end

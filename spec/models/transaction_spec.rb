263a935f8ce729883cf51b8ed34cebc599fb72f3
Jesus Romero <elherzt@gmail.com>
2013-10-24 18:54:21 -0500

    deleted tests to uuid scope

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -21,14 +21,6 @@ describe Transaction do
     (transactions.search("TangoSource").count).should eq(2)
   end
 
-  it "should find a transaction given a uuid" do
-    (transactions.uuid("d75b07fb-702d-4fe5-a89f-69b3db98cb1f").payee_name).should eq("Coopel")
-  end
-
-  it "when is passed a invalid uuid should return all records" do
-    (transactions.uuid("####-####-##-01").count).should eq(37)
-  end
-
   it "should return records with cleared value true " do
     (transactions.cleared.count).should eq(6)
   end

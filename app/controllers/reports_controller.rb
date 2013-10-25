ebfc2559c1dcb6081e4992dd711b9a52438c8e3b
Juan Carlos <juancarlos@tangosource.com>
2013-10-25 17:36:39 -0500

    refactor controller

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,19 +1,13 @@
 class ReportsController < ApplicationController
 
   def income_v_expense
-    @report = Transaction.all.group(:pm_type).sum(:amount)
-    respond_to do |format|
-      format.html
-      format.json { render json: income_v_expense_json }
-    end
+    @report = Transaction.group(:pm_type).sum(:amount)
+    format_respond
   end
 
   def spending_by_payee
-    @report = Transaction.all.group(:payee_id).sum(:amount)
-    respond_to do |format|
-      format.html
-      format.json { render json: spending_by_payee_json }
-    end
+    @report = Transaction.group(:payee_id).sum(:amount)
+    format_respond
   end
 
   def net_worth
@@ -21,67 +15,22 @@ class ReportsController < ApplicationController
     Transaction.net_worth.each do |e|
       @report[e.month] = e.amount.to_f
     end
-    respond_to do |format|
-      format.html
-      format.json { render json: net_worth_json }
-    end
+    format_respond
   end          
                
   def spending_by_category
-    @report = Transaction.all.group(:category_id).sum(:amount)
+    @report = Transaction.group(:category_id).sum(:amount)
+    format_respond
+  end
+
+  def format_respond
     respond_to do |format|
       format.html
-      format.json { render json: spending_by_category_json }
+      format.json { render json: json_for_chart }
     end
   end
 
-  def income_v_expense_json
-    {
-      labels: @report.keys,
-      datasets: [
-        {
-          fillColor: "rgba(220,220,220,0.5)",
-          strokeColor: "rgba(220,220,220,1)",
-          fillColor: "rgba(225, 0, 0, 0.5)",
-          strokeColor: "rgba(220, 220, 220, 1)",
-          data: @report.values
-        }
-      ]
-    }
-  end
-
-  def spending_by_payee_json
-    {
-      labels: @report.keys,
-      datasets: [
-        {
-          fillColor: "rgba(220,220,220,0.5)",
-          strokeColor: "rgba(220,220,220,1)",
-          fillColor: "rgba(225, 0, 0, 0.5)",
-          strokeColor: "rgba(220, 220, 220, 1)",
-          data: @report.values
-        }
-      ]
-    }
-  end
-
-  def spending_by_category_json
-    {
-      labels: @report.keys,
-      datasets: [
-        {
-          fillColor: "rgba(220,220,220,0.5)",
-          strokeColor: "rgba(220,220,220,1)",
-          fillColor: "rgba(225, 0, 0, 0.5)",
-          strokeColor: "rgba(220, 220, 220, 1)",
-          data: @report.values
-        }
-      ]
-    }
-
-  end
-
-  def net_worth_json 
+  def json_for_chart
     {
       labels: @report.keys,
       datasets: [

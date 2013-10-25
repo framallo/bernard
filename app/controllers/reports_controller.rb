18a30d63b93a6786572093d5c4f01a0b277988bc
Juan Carlos <juancarlos@tangosource.com>
2013-10-25 15:37:52 -0500

    created queries for get the correct values

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,16 +2,75 @@ class ReportsController < ApplicationController
 
   def income_v_expense
     @report = Transaction.all.group(:pm_type).sum(:amount)
+    respond_to do |format|
+      format.html
+      format.json { render json: income_v_expense_json }
+    end
   end
 
   def spending_by_payee
+    @report = Transaction.all.group(:payee_id).sum(:amount)
+    respond_to do |format|
+      format.html
+      format.json { render json: spending_by_payee_json }
+    end
   end
 
   def net_worth
-    @report = Transaction.where("account_id = 1 and date between '2013-01-01' and '2014-01-01'").sum(:amount).to_f
-  end
-
+    @report =  Transaction.select(:account_id, :amount).group(:account_id).sum(:amount)
+  end          
+               
   def spending_by_category
+    @report = Transaction.all.group(:category_id).sum(:amount)
+    respond_to do |format|
+      format.html
+      format.json { render json: spending_by_category_json }
+    end
   end
 
+  def income_v_expense_json
+    {
+      labels: @report.keys,
+      datasets: [
+        {
+          fillColor: "rgba(220,220,220,0.5)",
+          strokeColor: "rgba(220,220,220,1)",
+          fillColor: "rgba(225, 0, 0, 0.5)",
+          strokeColor: "rgba(220, 220, 220, 1)",
+          data: @report.values
+        }
+      ]
+    }
+  end
+
+  def spending_by_payee_json
+    {
+      labels: @report.keys,
+      datasets: [
+        {
+          fillColor: "rgba(220,220,220,0.5)",
+          strokeColor: "rgba(220,220,220,1)",
+          fillColor: "rgba(225, 0, 0, 0.5)",
+          strokeColor: "rgba(220, 220, 220, 1)",
+          data: @report.values
+        }
+      ]
+    }
+  end
+
+  def spending_by_category_json
+    {
+      labels: @report.keys,
+      datasets: [
+        {
+          fillColor: "rgba(220,220,220,0.5)",
+          strokeColor: "rgba(220,220,220,1)",
+          fillColor: "rgba(225, 0, 0, 0.5)",
+          strokeColor: "rgba(220, 220, 220, 1)",
+          data: @report.values
+        }
+      ]
+    }
+
+  end
 end

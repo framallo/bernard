44827722a71de226de3b6d084a0edc5cb3f89aad
Juan Carlos <juancarlos@tangosource.com>
2013-10-28 17:48:05 -0600

    making some conigurations over the tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 require 'factory_girl_rails'
+require 'capybara/rspec'
+require 'capybara/firebug'
 
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
@@ -15,7 +17,19 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
   config.infer_base_class_for_anonymous_controllers = false
+  config.treat_symbols_as_metadata_keys_with_true_values = true
 
   config.order = "random"
 
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end

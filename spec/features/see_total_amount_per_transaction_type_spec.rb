9e62b7aae7f3dff0bec1fc3ba6be890629c19d85
Jesus Romero <elherzt@gmail.com>
2013-10-30 12:56:49 -0600

    cretaed file to transaction type tests

---
+++ spec/features/see_total_amount_per_transaction_type_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+feature 'check total amount per transaction type' do
+
+  scenario "Deposit Transaction" do 
+  end
+
+  scenario "Transfer Transaction" do 
+  end
+
+  scenario "Widthdrawals Transaction" do 
+  end
+
+end

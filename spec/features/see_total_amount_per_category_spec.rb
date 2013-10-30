19c464afbeeaf591cb26594df3ab4884249474e3
Jesus Romero <elherzt@gmail.com>
2013-10-30 12:08:27 -0600

    added test for transactions per ccategories

--- spec/features/see_total_amount_per_category_spec.rb
+++ spec/features/see_total_amount_per_category_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 feature 'check total amount per category' do
+
   scenario 'Per category' do 
     visit root_path
     click_link "This Year"
@@ -13,6 +14,50 @@ feature 'check total amount per category' do
     page.should have_content('$0.00')
   end
 
+  scenario "Salario Category" do 
+    visit root_path
+    click_link "This Year"
+    click_link "Salario"
+    page.should have_content("10/16/13")
+    page.should have_content("10/31/13")
+    page.should have_content("$1,750.00")
+    page.should have_content("Deposit")
+    page.should have_content("$3,500.00")
+  end
 
+  scenario "Personal Category" do 
+    visit root_path
+    click_link "This Year"
+    click_link "Personal"
+    page.should have_content("10/18/13")
+    page.should have_content("$6,500.00")
+    page.should have_content("10/17/13")
+    page.should have_content("$2,400.00")
+    page.should have_content("Deposit")
+    page.should have_content("$9,000.00")
+    page.should have_content("Widthdrawals")
+    page.should have_content("$100.00")
+  end
 
+  scenario "Automovil Category" do 
+    visit root_path
+    click_link "This Year"
+    click_link "Automóvil"
+    page.should have_content("10/17/13")
+    page.should have_content("-$200.00")
+   page.should have_content("Widthdrawals")
+    page.should have_content("$200.00")
+  end
+
+  scenario "Caridad Category" do 
+    visit root_path
+    click_link "This Year"
+    click_link "Caridad"
+    page.should have_content("10/18/13")
+    page.should have_content("-$1,000.00")
+    page.should have_content("Transfer")
+    page.should have_content("$1,800.00")
+    page.should have_content("Widthdrawals")
+    page.should have_content("$1,000.00")
+  end
 end

f573e186b58362f4b8365d1685ad759a8908c4b8
Jesus Romero <elherzt@gmail.com>
2013-10-31 11:31:24 -0600

    add tests

--- spec/features/see_total_amount_per_transaction_type_spec.rb
+++ spec/features/see_total_amount_per_transaction_type_spec.rb
@@ -1,13 +1,44 @@
 require 'spec_helper'
 feature 'check total amount per transaction type' do
 
+  before(:each) do
+    visit root_path
+    click_link "This Year"
+  end
+
   scenario "Deposit Transaction" do 
+    click_link "Deposit"
+    page.should have_content("10/16/13")
+    page.should have_content("$1,750.00")
+    page.should have_content("10/17/13")
+    page.should have_content("$2,500.00")
+    page.should have_content("10/18/13")
+    page.should have_content("$6,600.00")
+    page.should have_content("10/31/13")
+    page.should have_content("$1,750.00")
+    page.should have_content("$12,600.00")
   end
 
   scenario "Transfer Transaction" do 
-  end
+    click_link "Transfer"
+    page.should have_content("10/17/13")
+    page.should have_content("-$1,250.00")
+    page.should have_content("10/18/13")
+    page.should have_content("-$3,250.00")
+    page.should have_content("10/23/13")
+    page.should have_content("-$500.00")
+    page.should have_content("$5,000.00")
+ end
 
   scenario "Widthdrawals Transaction" do 
-  end
+    click_link "Widthdrawals"     
+    page.should have_content("10/07/13")
+    page.should have_content("-$2,500.00")
+    page.should have_content("10/17/13")
+    page.should have_content("-$610.00")
+    page.should have_content("10/18/13")
+    page.should have_content("-$1,000.00")
+    page.should have_content("$4,110.00")
+ end
 
 end

b4e730acff0ab4e6e039bbd74f49c1bd785023c2
Jesus Romero <elherzt@gmail.com>
2013-10-31 16:44:20 -0600

    refactored tests

--- spec/features/see_total_amount_per_category_spec.rb
+++ spec/features/see_total_amount_per_category_spec.rb
@@ -4,6 +4,7 @@ feature 'check total amount per category' do
     visit root_path
     click_link "This Year"
   end
+
   scenario 'Per category' do 
     page.should have_content('Salario')
     page.should have_content('$3,500.00')
@@ -17,40 +18,49 @@ feature 'check total amount per category' do
 
   scenario "Salario Category" do 
     click_link "Salario"
-    page.should have_content("10/16/13")
-    page.should have_content("10/31/13")
-    page.should have_content("$1,750.00")
     page.should have_content("Deposit")
     page.should have_content("$3,500.00")
+    within('table') do
+      page.should have_content("10/16/13")
+      page.should have_content("10/31/13")
+      page.should have_content("$1,750.00")
+    end
   end
 
   scenario "Personal Category" do 
     click_link "Personal"
-    page.should have_content("10/18/13")
-    page.should have_content("$6,500.00")
-    page.should have_content("10/17/13")
-    page.should have_content("$2,400.00")
     page.should have_content("Deposit")
     page.should have_content("$9,000.00")
     page.should have_content("Widthdrawals")
     page.should have_content("$100.00")
+    within('table') do
+      page.should have_content("10/18/13")
+      page.should have_content("$6,500.00")
+      page.should have_content("10/17/13")
+      page.should have_content("$2,400.00")
+    end
   end
 
   scenario "Automovil Category" do 
     click_link "Automóvil"
-    page.should have_content("10/17/13")
-    page.should have_content("-$200.00")
     page.should have_content("Widthdrawals")
     page.should have_content("$200.00")
+    within('table') do
+      page.should have_content("10/17/13")
+      page.should have_content("-$200.00")
+    end
   end
 
   scenario "Caridad Category" do 
     click_link "Caridad"
-    page.should have_content("10/18/13")
-    page.should have_content("-$1,000.00")
     page.should have_content("Transfer")
     page.should have_content("$1,800.00")
     page.should have_content("Widthdrawals")
     page.should have_content("$1,000.00")
+    within('table') do
+      page.should have_content("10/18/13")
+      page.should have_content("-$1,000.00")
+    end
   end
+
 end

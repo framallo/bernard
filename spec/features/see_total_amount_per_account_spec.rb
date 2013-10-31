1c4a56d69f41544ce765081e2e9b629511610036
Jesus Romero <elherzt@gmail.com>
2013-10-31 16:20:59 -0600

    refactored tests

--- spec/features/see_total_amount_per_account_spec.rb
+++ spec/features/see_total_amount_per_account_spec.rb
@@ -8,50 +8,58 @@ feature "check total amount per account" do
   scenario "cheack all categories" do
     page.should have_content("Efectivo")
     page.should have_content("$550.00")
-    page.should have_content("$4,490.00")
-    page.should have_content("Tarjeta")
-    page.should have_content("-$250")
-    page.should have_content("$3,050.00")
-    page.should have_content("Cuenta Ahorro")
-    page.should have_content("$0.00")
-    page.should have_content("$950.00")
+    within ('table') do
+      page.should have_content("$4,490.00")
+      page.should have_content("Tarjeta")
+      page.should have_content("-$250")
+      page.should have_content("$3,050.00")
+      page.should have_content("Cuenta Ahorro")
+      page.should have_content("$0.00")
+      page.should have_content("$950.00")
+    end
   end
 
   scenario "Check Efectivo" do
     click_link "Efectivo"
     page.should have_content("(17)")
     page.should have_content("$4,490.00")
-    page.should have_content("10/16/13")
-    page.should have_content("$1,750.00")
-    page.should have_content("10/17/13")
-    page.should have_content("$640.00")
-    page.should have_content("10/18/13")
-    page.should have_content("$150.00")
-    page.should have_content("10/23/13")
-    page.should have_content("$500.00")
-    page.should have_content("10/31/13")
-    page.should have_content("$1,750.00")
+    within('table') do
+      page.should have_content("10/16/13")
+      page.should have_content("$1,750.00")
+      page.should have_content("10/17/13")
+      page.should have_content("$640.00")
+      page.should have_content("10/18/13")
+      page.should have_content("$150.00")
+      page.should have_content("10/23/13")
+      page.should have_content("$500.00")
+      page.should have_content("10/31/13")
+      page.should have_content("$1,750.00")
+    end
   end
 
   scenario "Check Tarjeta" do
     click_link "Tarjeta"
     page.should have_content("(11)")
     page.should have_content("$3,050.00")
-    page.should have_content("10/07/13")
-    page.should have_content("-$2,500.00")
-    page.should have_content("10/17/13")
-    page.should have_content("$1,250.00")
-    page.should have_content("10/18/13")
-    page.should have_content("$4,300.00")
+    within ('table') do
+      page.should have_content("10/07/13")
+      page.should have_content("-$2,500.00")
+      page.should have_content("10/17/13")
+      page.should have_content("$1,250.00")
+      page.should have_content("10/18/13")
+      page.should have_content("$4,300.00")
+    end
   end
-  
+
   scenario "Cuenta Ahorro" do
     click_link "Cuenta Ahorro"
     page.should have_content("(9)")
     page.should have_content("$950.00")
-    page.should have_content("10/18/13")
-    page.should have_content("$1,450.00")
-    page.should have_content("10/23/13")
-    page.should have_content("-$500.00")
- end
+    within ('table') do
+      page.should have_content("10/18/13")
+      page.should have_content("$1,450.00")
+      page.should have_content("10/23/13")
+      page.should have_content("-$500.00")
+    end
+  end
 end

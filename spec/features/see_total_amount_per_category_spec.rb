ea9ee78da896fed35fd156db9007aa7a8ea88fd5
Jesus Romero <elherzt@gmail.com>
2013-10-31 11:30:18 -0600

    refactored tests

--- spec/features/see_total_amount_per_category_spec.rb
+++ spec/features/see_total_amount_per_category_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 feature 'check total amount per category' do
-
-  scenario 'Per category' do 
+  before(:each) do
     visit root_path
     click_link "This Year"
+  end
+  scenario 'Per category' do 
     page.should have_content('Salario')
     page.should have_content('$3,500.00')
     page.should have_content('Personal')
@@ -15,8 +16,6 @@ feature 'check total amount per category' do
   end
 
   scenario "Salario Category" do 
-    visit root_path
-    click_link "This Year"
     click_link "Salario"
     page.should have_content("10/16/13")
     page.should have_content("10/31/13")
@@ -26,8 +25,6 @@ feature 'check total amount per category' do
   end
 
   scenario "Personal Category" do 
-    visit root_path
-    click_link "This Year"
     click_link "Personal"
     page.should have_content("10/18/13")
     page.should have_content("$6,500.00")
@@ -40,18 +37,14 @@ feature 'check total amount per category' do
   end
 
   scenario "Automovil Category" do 
-    visit root_path
-    click_link "This Year"
     click_link "Automóvil"
     page.should have_content("10/17/13")
     page.should have_content("-$200.00")
-   page.should have_content("Widthdrawals")
+    page.should have_content("Widthdrawals")
     page.should have_content("$200.00")
   end
 
   scenario "Caridad Category" do 
-    visit root_path
-    click_link "This Year"
     click_link "Caridad"
     page.should have_content("10/18/13")
     page.should have_content("-$1,000.00")

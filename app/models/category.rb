e89751bbaa736b08d4260d46eb0d1f511a8b8602
Jesus Romero <elherzt@gmail.com>
2013-11-08 15:06:05 -0600

    created budgeted scope for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,12 @@
 class Category < ActiveRecord::Base
+  ## Budget period
+  # 1 Semanal
+  # 2 Mensual
   scope :active,             ->       { where(deleted:false) }
   scope :transaction_ids,    ->(ids)  { joins(:splits => :transaction).where(transactions: {id: ids})                        }
   scope :transaction_totals, ->       { joins(:splits => :transaction).merge(Transaction.total_amount)                       }
   scope :group_by_name,      ->       { select('categories.name', 'categories.id').group('categories.name', 'categories.id') }
+  scope :budgeted,           ->       { where("budget_limit is not null and budget_period is not null ") }
 
   has_many :splits
 end

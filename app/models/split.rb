65a853c11525db32975efb33256f9ed72c868219
Jesus Romero <elherzt@gmail.com>
2013-11-11 15:23:29 -0600

    removed unnecesary code

--- app/models/split.rb
+++ app/models/split.rb
@@ -1,21 +1,9 @@
 class Split < ActiveRecord::Base
-  require 'filter'
 
   belongs_to :transaction
   belongs_to :category
 
   validates_associated :transaction, :category
 
-  scope :active, -> {Split.where({id: transaction_active_ids})}
-  scope :per_category, ->(id) {active.where(category_id: id)}
-  scope :interval, ->(from, to) { where("splits.date >= ? AND splits.date <= ?", from, to) }
-
-  def self.transaction_active_ids
-    Transaction.active.map(&:id)
-  end
-
-  def self.filter(conditions)
-    Filter.new(conditions)
-  end
 
 end

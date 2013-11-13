d09b29d9e48ce07caef180fb307a562c700e818b
Jesus Romero <elherzt@gmail.com>
2013-11-13 16:46:02 -0600

    fixed problem with dates

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -1,4 +1,5 @@
 module BudgetsHelper
+ 
   def status(budget)
     total = total_category(budget) *-1 
     if total > budget_budgeted(budget)
@@ -12,18 +13,36 @@ module BudgetsHelper
   
   def total_category(budget)
     total = @filter.budgets_sum(budget.id) 
-    total.round(2)
+    total.round(0)
   end
 
   def budget_available(budget)
     available = budget_budgeted(budget) - total_category(budget)*-1 
     available = 0 if available < 0
-    available.round(2)
+    available.round(0)
   end
 
   def budget_balance(budget)
     balance = budget_budgeted(budget) - total_category(budget)*-1 
-    balance.round(2)
+    balance.round(0)
+  end
+
+  def available_percentage(budget)
+    unless status(budget) == "danger" or status(budget) == "warning"
+      percentage = (budget_available(budget) * 100 / budget_budgeted(budget))
+      percentage = 100 if percentage > 100
+      percentage
+    else
+      100
+    end
+  end
+
+  def spent_percentage(budget) 
+    unless status(budget) == "danger" or status(budget) == "warning"
+      ((total_category(budget)*-1) * 100 / budget_budgeted(budget))
+    else
+      0
+    end
   end
 
   def budget_budgeted(budget)
@@ -40,40 +59,40 @@ module BudgetsHelper
   def solve_budgeted_per_week(budget)
     case budget.budget_period
     when 0
-      (budget.budget_limit * 7).round(2)
+      (budget.budget_limit * 7).round(0)
     when 2
-      weeks = Time::days_in_month(Time.now.month, Time.now.year).to_f / 7
-      (budget.budget_limit / weeks).round(2)
+      weeks = Time::days_in_month(@filter.from.month, @filter.from.year).to_f / 7
+      (budget.budget_limit / weeks).round(0)
     when 3
       # to do
     when 4
-      (budget.budgeted_limit / 52).round(2) 
+      (budget.budgeted_limit / 52.142857143).round(0) 
     end
   end
 
   def solve_budgeted_per_month(budget)
     case budget.budget_period
     when 0
-      days =  Time::days_in_month(Time.now.month, Time.now.year)
-      (budget.budget_limit * days).round(2)
+      days =  Time::days_in_month(@filter.from.month, @filter.from.year)
+      (budget.budget_limit * days).round(0)
     when 1
-      weeks = Time::days_in_month(Time.now.month, Time.now.year).to_f / 7
-      (budget.budget_limit * weeks).round(2)
+      weeks = Time::days_in_month(@filter.from.month, @filter.from.year).to_f / 7
+      (budget.budget_limit * weeks).round(0)
     when 3
       # to do
     when 4
-      (budget.budget_limit / 12).round(2)
+      (budget.budget_limit / 12).round(0)
     end
   end
 
   def solve_budgeted_per_year(budget)
     case budget.budget_period
     when 0
-      (budget.budget_limit * 365).round(2)
+      (budget.budget_limit * 365).round(0)
     when 1
-      (budget.budget_limit * 52).round(2)
+      (budget.budget_limit * 52.142857143).round(0)
     when 2
-      (budget.budget_limit * 12).round(2)
+      (budget.budget_limit * 12).round(0)
     when 3
       # to do
     end
@@ -82,7 +101,7 @@ module BudgetsHelper
   def set_period
     case @filter.kind
     when "month"
-      @filter.from.strftime("%B")
+      @filter.from.strftime("%B %Y")
     when "week"
       "#{@filter.from.strftime('%d %B %Y')} - #{@filter.to.strftime('%d %B %Y')}"
     when "year"

79a3b10fb0196fe956dc04d74c5ca98cf4b7084d
Susana Ramallo <ramallo.susana@gmail.com>
2013-12-04 18:45:02 -0600

    added name in categories

--- app/models/split.rb
+++ app/models/split.rb
@@ -1,3 +1,4 @@
 class Split< ActiveRecord::Base
   belongs_to :transaction
+  belongs_to :category
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,17 @@
 class Transaction < ActiveRecord::Base
   belongs_to :account
   has_many :splits
+  has_many :categories, :through => :splits
 
   attr_accessor :balance
 
   scope :active,  -> { where.not(deleted: true, pm_type: 5, account_id: 0)}
+  scope :everything,  -> { includes(:account, :categories) }
+  scope :full,  -> { active.everything}
+
+
+  def category_names
+    categories.map {|c| c.name }.join(",")
+  end
+
 end

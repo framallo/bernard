da4550ea88b0158703e8a7b6ca64d3024682ef84
Jesus Romero <elherzt@gmail.com>
2013-12-05 17:26:00 -0600

    decorator to budgets category

--- app/decorators/budget_category_decorator.rb
+++ app/decorators/budget_category_decorator.rb
@@ -1,13 +1,40 @@
 class BudgetCategoryDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def dates_intervals(from, to)
+    @@from = from
+    @@to   = to+1
+  end
+
+  def amount
+    amount = Category::BudgetCategory.interval(@@from, @@to)
+    amount = amount.sum_category(model.id) 
+    amount < 0 ? amount*-1 : amount
+  end
+
+  def limit
+    (model.amount_day * ((@@to - @@from).to_i+1)).round(0)
+  end
+
+  def balance
+    (limit - amount).round(0)
+  end
+
+  def available
+    balance < 0 ? 0 : balance
+  end
+
+  def color 
+    balance < 0 ? "danger" : "warning"
+  end
+
+  def spent_percentage
+    percentage = (amount * 100) / limit
+    percentage < 100 ? percentage : 100
+  end
+
+  def available_percentage
+    100 - spent_percentage
+  end
 
 end

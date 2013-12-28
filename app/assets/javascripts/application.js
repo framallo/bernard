c67b422ba67c826e6a3debd378dce441af0305f4
Federico Ramallo <framallo@gmail.com>
2013-12-28 15:50:51 -0600

    added chosen select plugin

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require jquery-ui-timepicker-addon
+//= require chosen-jquery
+//= require chosen
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .

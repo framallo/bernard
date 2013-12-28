261004071f0458e75234b8e5d1b49d8fcfbca3de
Federico Ramallo <framallo@gmail.com>
2013-12-28 15:37:29 -0600

    added time picker to transaction

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.all
+//= require jquery-ui-timepicker-addon
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .

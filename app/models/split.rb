9ea81dc48c34f02af092b7a0b2f10c88853664e3
Federico Ramallo <framallo@gmail.com>
2015-06-05 11:53:41 -0500

    renamed transaction association in split

--- app/models/split.rb
+++ app/models/split.rb
@@ -1,7 +1,7 @@
 class Split < ActiveRecord::Base
   scope :with_amount, -> { where('amount<>0') }
 
-  belongs_to :transaction
+  belongs_to :split_transaction, :foreign_key => 'transaction_id', :class_name => "Transaction"
   belongs_to :category
   belongs_to :department, foreign_key: "class_id"
   belongs_to :account, foreign_key: "transfer_to_account_id"

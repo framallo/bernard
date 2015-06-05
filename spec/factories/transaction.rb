125fc444da904b9c3f1db6bb0774d285cae04bcd
Federico Ramallo <framallo@gmail.com>
2015-06-05 11:54:25 -0500

    removed split factory temporarily

--- spec/factories/transaction.rb
+++ spec/factories/transaction.rb
@@ -34,17 +34,17 @@ end
 transaction = FactoryGirl.create(:transaction) 
 
 FactoryGirl.define do
-  factory :split do
-    pm_id                    transaction.pm_id
-    transaction_id           transaction.id
-    amount                   transaction.amount
-    xrate                    (1..10).to_a.sample
-    category_id              transaction.category_id
-    class_id                 (0..10).to_a.sample
-    memo                     Faker::Lorem.paragraph
-    transfer_to_account_id   (1..100).to_a.sample
-    currency_code            currency.sample
-    of_x_id                  "dummy"
-  end
+  #factory :split do
+    #pm_id                    transaction.pm_id
+    #transaction_id           transaction.id
+    #amount                   transaction.amount
+    #xrate                    (1..10).to_a.sample
+    #category_id              transaction.category_id
+    #class_id                 (0..10).to_a.sample
+    #memo                     Faker::Lorem.paragraph
+    #transfer_to_account_id   (1..100).to_a.sample
+    #currency_code            currency.sample
+    #of_x_id                  "dummy"
+  #end
 end
 
